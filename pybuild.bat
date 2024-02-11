0968dc306576b7354e1df77c69768ffcced73a18
Gadg8eer <kidpunk@gadg8eer.top>
2024-02-11 01:17:24 -0700

    Setup changes

--- pybuild.bat
+++ pybuild.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 @RD /S /Q ".\.nmlcache"
 @RD /S /Q ".\backups"
-@DEL /S /Q ".\IH3_AddonSet.grf"
-@DEL /S /Q ".\IH3_AddonSet.nml"
+@DEL /S /Q ".\quicksilver.grf"
+@DEL /S /Q ".\quicksilver.nml"
 :start
-python nml_patcher.py -f "IH3_AddonSet.pnml" -o "IH3_AddonSet.nml" -b 1 -v 1
-nmlc IH3_AddonSet.nml -o IH3_AddonSet.grf
+python nml_patcher.py -f "quicksilver.pnml" -o "quicksilver.nml" -b 1 -v 1
+nmlc quicksilver.nml -o quicksilver.grf
 PAUSE
